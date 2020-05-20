f36989c66330691c88a2a8565264b86e7b5a8f38
georgette-a <45202432+georgette-a@users.noreply.github.com>
2020-05-20 22:45:26 +0000

    Update db_cred.php

--- controller/model/db_cred.php
+++ controller/model/db_cred.php
@@ -1,11 +1,11 @@
 <?php
 
-    define("SERVER", "localhost");
+    define("SERVER", "ec2-18-235-20-228.compute-1.amazonaws.com");
     
-    define("USERNAME", "root");
+    define("USERNAME", "zdwvppwltrwyvo");
     
-    define("PASSWORD", "");
+    define("PASSWORD", "182c06316e4cc9c550b380e0b8a789e1d2fdcfcbe4a7c6edeca88810200eae6b");
     
-    define("DATABASE", "youcheckedin");
+    define("DATABASE", "d4k8oqgpp097iv");
 
 ?>
